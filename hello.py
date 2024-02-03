c1fba558c66d2e5e5a36574b0ca64f5d3fc11db3
RMFT251 <151698126+RMFT251@users.noreply.github.com>
2024-02-03 13:58:13 -0600

    108-s1

---
+++ hello.py
@@ -0,0 +1,63 @@
+print ("hello world")
+var=10
+print (var)
+
+#variables
+x=5
+y=3
+
+#operations
+sum= x+y
+print (sum)
+
+#string operations
+name="bigmeech"
+greeting = "Hello" + name + "!"
+print (greeting)
+
+#list //this is the arrays
+numbers= [1,2,3,4,5]
+
+#loop
+for number in numbers:
+    print ("you're an adult")
+else:
+    print ("you're an minor")
+#(i=0; i<numbers; i++
+# let number = numbers[
+# console.log(number)
+
+#JS comparative
+#function(){
+    # console.log("function");
+#}
+
+#Define the function in python
+def greet(name):
+    return "Hello" + name
+
+#Call the function
+result= greet("bigmeech")
+print (result)
+
+example = 1
+example2 = "2"
+
+print (example + int(example2))
+
+#function to calculate the power of a number
+def power(base, exponent=2):
+    return base ** exponent
+#call the function
+result = power(2)
+result2 = power(3,3)
+#print result
+print (result)
+print (result2)
+##Create a function that multiply 2 parameters (numbers) 
+def multiply (number1, number2):
+    multiplyResult = number1*number2
+    return multiplyResult
+#call the function
+product = multiply(12,12)
+print (product)
